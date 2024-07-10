35eebf1d63fc903cf015f40aebf9c391ae4bfd61
pouryourlove <lqu134@naver.com>
2024-07-10 18:48:42 +0900

    main page & modal edited

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const firstSwiper = new Swiper(".first-swiper", {
 });
 
 const secondSwiper = new Swiper(".second-swiper", {
-  slidesPerView: 4,
+  slidesPerView: 6,
+  spaceBetween: 0,
   loop: true,
   navigation: {
   nextEl: ".swiper-button-next",
@@ -30,3 +31,26 @@ const secondSwiper = new Swiper(".second-swiper", {
 
 
 
+// modal
+
+const loginBtn = document.querySelector('#openLoginModal');
+const modal = document.querySelector('.loginModal');
+const closeBtn = document.querySelector('.close');
+const overlay = document.querySelector('.overlay');
+
+loginBtn.addEventListener('click',function(){
+  modal.classList.toggle('hidden')
+  overlay.classList.toggle('hidden')
+})
+
+closeBtn.addEventListener('click', function(){
+  modal.classList.toggle('hidden')
+  overlay.classList.toggle('hidden')
+})
+
+document.addEventListener('keydown',function(e){
+  if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+    modal.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
+  }
+})
